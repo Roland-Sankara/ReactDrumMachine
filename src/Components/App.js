96b216f03505d5df13d16defb125286fbc42898d
Roland-Sankara <roland1sankara@gmail.com>
2021-05-28 15:23:11 +0300

    fix(drum click and key press events)

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,6 +4,18 @@ import Display from './Display';
 
 import '../Styles/App.css';
 
+const pads = [
+    {key:'Q',keyCode:81,clip:'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',clipName:'Heater-1'},
+    {key:'W',keyCode:87,clip:'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',clipName:'Heater-2'},
+    {key:'E',keyCode:69,clip:'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',clipName:'Heater-3'},
+    {key:'A',keyCode:65,clip:'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',clipName:'Heater-4_1'},
+    {key:'S',keyCode:83,clip:'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',clipName:'Heater-6'},
+    {key:'D',keyCode:68,clip:'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',clipName:'Dsc_Oh'},
+    {key:'Z',keyCode:90,clip:'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',clipName:'Kick_n_Hat'},
+    {key:'X',keyCode:88,clip:'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',clipName:'RP4_KICK_1'},
+    {key:'C',keyCode:67,clip:'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',clipName:'Cev_H2'}
+];
+
 function App(){
     const [text,setText] = useState("-- Click & Display --");
 
@@ -14,7 +26,7 @@ function App(){
     return(
         <div id="drum-machine">
             <Display text={text}/>
-            <Drumbox textFunc={handleChange}/>
+            <Drumbox textFunc={handleChange} clips={pads} />
         </div>
     );
 };
--- src/Components/Drumpad.js
+++ src/Components/Drumpad.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 function Drumpad(props){
-    function clickFunctions(e){
-        props.play.funcOne(e);
-        props.play.funcTwo(props.drum.clipName)
+
+    function playAudio(event){
+        const audioElement = document.querySelector(`#${event.target.innerText}`);
+        audioElement.currentTime = 0;
+        audioElement.play();
     }
+
+    function displayClickText(event){
+        playAudio(event);
+        props.clipText(props.audioClip.clipName);
+    }
+
+    function handleKeyDown(event){
+        const audioElement = document.querySelector(`#${props.audioClip.key}`);
+        if(event.keyCode === props.audioClip.keyCode){
+            audioElement.currentTime = 0;
+            audioElement.play();
+        }
+    }
+
+    // listen for keydown events then invoke the handleKeyDown function
+    useEffect(() => {
+        document.addEventListener('keydown',handleKeyDown);
+        return()=>{
+            document.removeEventListener('keydown',handleKeyDown);
+        }
+    },[])
+
     return(
-        <div className="drum-pad" onClick={clickFunctions} id={props.drum.clipName}>
-            {props.drum.key}
-            <audio src={props.drum.clip} className="clip" id={props.drum.key}  />
+        <div className="drum-pad" onClick={displayClickText} id={props.audioClip.clipName}>
+            {props.audioClip.key}
+            <audio src={props.audioClip.clip} className="clip" id={props.audioClip.key}  />
         </div>
     )
 };
